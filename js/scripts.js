1b762099549e89c663f9208a366a1e95e3327c57
TsukisaLuna <kuoc1234@naver.com>
2024-01-27 13:15:45 +0900

    Edit

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function acyncMovePage(url){
+        // ajax option
+        var ajaxOption = {
+                url : url,
+                async : true,
+                type : "POST",
+                dataType : "html",
+                cache : false
+        };
+        
+        $.ajax(ajaxOption).done(function(data){
+            // Contents 영역 삭제
+            $('#main').children().remove();
+            // Contents 영역 교체
+            $('#main').html(data);
+        });
+    }
